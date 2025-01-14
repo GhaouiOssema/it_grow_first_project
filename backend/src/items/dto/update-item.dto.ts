9483468e09e9888d346e484ff72bb2dd2738e429
OUSSEMA_GHAOUI28 <75329850+GhaouiOssema@users.noreply.github.com>
2025-01-14 18:23:56 +0100

    changes

--- backend/src/items/dto/update-item.dto.ts
+++
@@ -1,8 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateItemDto } from './create-item.dto';
-
-export class UpdateItemDto extends PartialType(CreateItemDto) {
-  readonly name?: string;
-  readonly description?: string;
-  readonly price?: number;
-}
