d80a848bde10b7c5d6545ecfb126ebdf0c0853c6
OUSSEMA_GHAOUI28 <75329850+GhaouiOssema@users.noreply.github.com>
2025-01-21 05:13:18 +0100

    changes

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+import * as Handlebars from 'handlebars';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  DisplayMessage(): string {
+    const filePath = join(__dirname, '..', 'templates', 'welcome.html');
+    const fileContent = readFileSync(filePath, 'utf8');
+
+    const data = {
+      status: 'Application is live 🎉',
+      currentDate: new Date().toLocaleString(),
+      year: new Date().getFullYear(),
+      deploymentDate: new Date().toLocaleString(),
+    };
+
+    const template = Handlebars.compile(fileContent);
+
+    return template(data);
   }
 }
